dd49bcfb7e1132e66348926a6439e9561526773b
John Doe <johndoe@example.com>
2021-04-21 11:04:21 +0200

    first java file

---
+++ src/test/java/TestArbreBinaire.java
@@ -0,0 +1,9 @@
+public class TestArbreBinaire {
+
+	public static void main(String[] args) {
+	/**
+	 *  chat : mange() lait     faireunBruit() miauler
+	 *  chien : mange() mollet                 aboyer
+	 */
+	}
+}

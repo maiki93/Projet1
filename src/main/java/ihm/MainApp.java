347b26b86745562257ca0e03b2d1dda0e95c6a4a
Paul Cuquémy <p.cuquemy@gmail.com>
2021-04-26 16:10:04 +0200

    function add rewriteOneNode

--- src/main/java/ihm/MainApp.java
+++ src/main/java/ihm/MainApp.java
@@ -1,35 +1,51 @@
 package main.java.ihm;
 
+import java.io.IOException;
 import java.util.List;
 
 import javafx.application.Application;
 import main.java.Admin;
 import main.java.AdminDAO;
+import main.java.ArbreBinaire;
+import main.java.NodeStagiaire;
+import main.java.Stagiaire;
+import main.java.StagiaireDAO;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 	public static void main(String[] args) {
-		AdminDAO daoadmin = new AdminDAO();
-		daoadmin.readTxtFichier();
-		List<Admin> adminList=daoadmin.getAdminList();
-		for (Admin admin : adminList) {
-			System.out.println(admin.getNom()+admin.getMotDePasse());
-		}
-		Admin adminRoot = new Admin("root", "roote");
-		daoadmin.trytoFind(adminRoot);
+
+		Stagiaire stagiaire = new Stagiaire("alonzo", "paul", "94", "eql ai109", 2020);
+		// StagiaireDAO dao = new StagiaireDAO();
+		ArbreBinaire ab = new ArbreBinaire();
+//		try {
+//			System.out.println("test de text: " + ab.addStagiaire(stagiaire));
+//
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	
 		launch(args);
 	}
-	
+
 	@Override
-	public void start(Stage stage)throws Exception{
+	public void start(Stage stage) throws Exception {
 		MainPannel root = new MainPannel();
 		stage.setTitle("Annuaire Eql");
 		Scene scene = new Scene(root);
-		scene.getStylesheets().add("main/java/ihm/style.css");
+		System.out.println(getClass());
+		scene.getStylesheets().add("/main/java/ihm/style.css");
 		stage.setScene(scene);
 		stage.sizeToScene();
 		stage.show();
 	}
 
 }
+
+
+
+
+
+

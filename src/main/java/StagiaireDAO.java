f6bb648acb94bb6d4adfbc8865d3f2c61f1971e6
Paul Cuquémy <p.cuquemy@gmail.com>
2021-04-26 16:49:26 +0200

    foreach

--- src/main/java/StagiaireDAO.java
+++ src/main/java/StagiaireDAO.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;

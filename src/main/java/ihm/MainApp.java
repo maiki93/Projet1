c1fa944ea9cebaa3dbf92a07c5a4106c5b08ebde
Paul Cuquémy <p.cuquemy@gmail.com>
2021-04-30 15:51:25 +0200

    clear

--- src/main/java/ihm/MainApp.java
+++ src/main/java/ihm/MainApp.java
@@ -1,30 +1,22 @@
 package main.java.ihm;
 
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 	public static void main(String[] args) {
-
-		
 		launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage stage) throws Exception {
 		RootPanel root = new RootPanel();
 		stage.setTitle("Annuaire Eql");
 		Scene scene = new Scene(root);
-		// TODO
-		// scene.getStylesheets().add(System.getProperty("user.dir")+"/src/main/java/ihm/style.css");
 		scene.getStylesheets().add("main/java/ihm/style.css");
-		System.out.println(getClass());
 		stage.setScene(scene);
 		stage.sizeToScene();
 		stage.show();
 	}
-
 }
-
